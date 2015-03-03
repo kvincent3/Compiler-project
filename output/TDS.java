bf658de81301952d885941b5509e37d78f34706b
Ansaouiddine Aboudou <ansaouiddine.aboudou@telecomnancy.net>
2015-03-03 13:40:25 -0800

    Changement attributs TDS

--- output/TDS.java
+++ output/TDS.java
@@ -2,36 +2,29 @@ import java.util.ArrayList;
 
 
 public class TDS {
+	String nom;
 	ArrayList<Symbole> variable = new ArrayList<Symbole>();
-	ArrayList<Symbole> fonction = new ArrayList<Symbole>();
-	ArrayList<Symbole> procedure = new ArrayList<Symbole>();
 	
-	public TDS(ArrayList<Symbole> variable, ArrayList<Symbole> fonction,
-			ArrayList<Symbole> procedure) {
+	public TDS(String nom, ArrayList<Symbole> variable) {
 		super();
+		this.nom = nom;
 		this.variable = variable;
-		this.fonction = fonction;
-		this.procedure = procedure;
 	}
-	
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
 	public ArrayList<Symbole> getVariable() {
 		return variable;
 	}
+
 	public void setVariable(ArrayList<Symbole> variable) {
 		this.variable = variable;
 	}
-	public ArrayList<Symbole> getFonction() {
-		return fonction;
-	}
-	public void setFonction(ArrayList<Symbole> fonction) {
-		this.fonction = fonction;
-	}
-	public ArrayList<Symbole> getProcedure() {
-		return procedure;
-	}
-	public void setProcedure(ArrayList<Symbole> procedure) {
-		this.procedure = procedure;
-	}
-	
 	
 }
