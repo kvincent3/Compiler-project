9abdbc1fab5977d9d448f64b8bae59c767627308
Condere Jerome <jerome.condere@gmail.com>
2015-03-04 00:37:19 +0100

    rajout fct  pour avoir la liste des symbole de type var (pas complet)

--- output/TDS.java
+++ output/TDS.java
@@ -1,4 +1,7 @@
+
 import java.util.ArrayList;
+
+import org.antlr.runtime.tree.Tree;
 
 
 public class TDS {
@@ -26,5 +29,77 @@ public class TDS {
 	public void setVariable(ArrayList<Symbole> variable) {
 		this.variable = variable;
 	}
+	public void getSymboleVar(Tree ast,int prof,ArrayList<Symbole> l)
+	{
+		if(ast==null)
+		{
+			return ;
+		}
+		if(ast.getChildCount()==0)
+		{
+			System.out.println("feuille");
+			return;
+		}
+		if(ast.getText().equals("do"))
+		{
+			for(int i=0;i<ast.getChildCount();i++)
+			{
+				
+				if(ast.getChild(i).getText().equals("DECLARATION"))
+				{
+					System.out.println("fils declaration");
+					getSymboleVar(ast.getChild(i),1,l);
+					
+				}
+				if(ast.getChild(i).getText().equals("INSTRUCTION"))
+				{
+					System.out.println("fils instruction");
+					getSymboleVar(ast.getChild(i),1,l);
+				}
+			}
+		}
+		
+		if(ast.getText().equals("DECLARATION"))
+		{
+			for(int i=0;i<ast.getChildCount();i++)
+			{
+				Tree child_i=ast.getChild(i);
+				if(child_i.getText().equals("VAR"))
+				{
+					String type=child_i.getChild(0).getText();
+					System.out.println("variable(s) trouvée(s)");
+					System.out.print("type: "+type+" ");
+					for(int j=1;j<child_i.getChildCount();j++)
+					{
+					 String id=child_i.getChild(j).getText();
+					System.out.print("id: "+id+" ");
+					l.add(new Symbole(id,type,null,null,Integer.toString(prof+1)));
+					}
+					System.out.print("\n\n");
+				}
+				else
+				{
+					getSymboleVar(ast.getChild(i),prof,l);
+				}
+				
+			}
+		}
+		else if(ast.getText().equals("INSTRUCTION"))
+		{
+			for(int i=0;i<ast.getChildCount();i++)
+			{
+				
+				getSymboleVar(ast.getChild(i),prof,l);
+			}
+		}
+		
+		
+		;
+		return ;
+		
+		
+	}
+
 	
 }
+
